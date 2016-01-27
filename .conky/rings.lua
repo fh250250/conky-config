ce0b872e0194baaa9580113a67e6ffbd50115be2
gasper <gasper.duan@loweprofero.com>
2016-01-27 18:20:35 +0800

    init

---
+++ .conky/rings.lua
@@ -0,0 +1,45 @@
+rings_table = {
+    {
+        name='fs_used_perc',
+        arg='/',
+        max=100,
+        bg_colour=0xffffff,
+        bg_alpha=0.2,
+        fg_colour=0xc1c1c1,
+        fg_alpha=0.8,
+        x=400, y=100,
+        radius=80,
+        thickness=15,
+        start_angle=-90,
+        end_angle=180
+    },
+    {
+        name='memperc',
+        arg='',
+        max=100,
+        bg_colour=0xffffff,
+        bg_alpha=0.2,
+        fg_colour=0xc1c1c1,
+        fg_alpha=0.8,
+        x=400, y=100,
+        radius=60,
+        thickness=15,
+        start_angle=-90,
+        end_angle=180
+    },
+    {
+        name='cpu',
+        arg='cpu0',
+        max=100,
+        bg_colour=0xffffff,
+        bg_alpha=0.2,
+        fg_colour=0xc1c1c1,
+        fg_alpha=0.8,
+        x=400, y=100,
+        radius=40,
+        thickness=15,
+        start_angle=-90,
+        end_angle=180
+    },
+}
+
